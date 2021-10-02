ea77915d5f67c35133a99488a593da03de23c344
Skara <skara90@mail.ru>
2021-10-02 21:50:07 +0300

    feat: decide 10 new tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = str + '';
+  let repeatTimes = (options.repeatTimes || 1),
+      separator = (options.separator || '+'),
+      addition = (options.addition || '') + '',
+      additionSeparator = (options.additionSeparator || '|'),
+      additionRepeatTimes = (options.additionRepeatTimes || 1);
+
+  if (options.addition === false) {
+    addition = 'false';
+  } else if (options.addition === null) {
+    addition = 'null';
+  }
+
+  let arr = [],
+      additionArr = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+    arr.push(str);
+  }
+  if (addition) {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      additionArr.push(addition);
+    }
+  }
+  return arr.map((e) => e + additionArr.join(additionSeparator)).join(separator);
 }
